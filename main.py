5779fe4320271b82a14ddc3a979142ec7fc458fc
Varun <panjwanivarun12@gmail.com>
2024-09-04 16:03:45 -0400

    PLotted random line chart.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as sd
+import plotly.express as px
+from streamlit import plotly_chart
 
 sd.title("Weather Forecast for the next days.")
 place = sd.text_input("Place", placeholder="Enter the city.")
@@ -7,3 +9,8 @@ days = sd.slider("Forecast Days.", min_value=1, max_value=5,
 option = sd.selectbox("Selecet data to view.", ("Temperature","Sky"))
 
 sd.subheader(f"{option} for the next {days} days in {place}.")
+
+date = ["2024-04-12", "2024-04-13","2024-04-14"]
+temp = [12,13,14]
+figure = px.line(x=date, y= temp, labels={"x": "Dates", "y": "Temperature (C)"})
+sd.plotly_chart(figure)
