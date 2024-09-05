58398e53ec83c9d1bcaf24cb7e968d75f626d4e9
Varun <panjwanivarun12@gmail.com>
2024-09-05 13:29:08 -0400

    Final steps.

--- backend.py
+++ backend.py
@@ -2,18 +2,15 @@ import requests
 
 API_KEY ="a33a9d30deccb330c9daf36cb23e0cb4"
 
-def get_data(place,days=None,option=None):
+def get_data(place,days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * days
     filtered_data = filtered_data[:nr_values]
-    if option == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if option == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo", days=3, option="Temperature"))
+    print(get_data(place="Tokyo", days=3))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as sd
 import plotly.express as px
-from streamlit import plotly_chart
+from backend import get_data
 
 sd.title("Weather Forecast for the next days.")
 place = sd.text_input("Place", placeholder="Enter the city.")
@@ -10,7 +10,20 @@ option = sd.selectbox("Selecet data to view.", ("Temperature","Sky"))
 
 sd.subheader(f"{option} for the next {days} days in {place}.")
 
-date = ["2024-04-12", "2024-04-13","2024-04-14"]
-temp = [12,13,14]
-figure = px.line(x=date, y= temp, labels={"x": "Dates", "y": "Temperature (C)"})
-sd.plotly_chart(figure)
+if place:
+    try:
+        filtered_data = get_data(place,days)
+        if option == "Temperature":
+            temp = [i["main"]["temp"] / 10 for i in filtered_data]
+            date = [i["dt_txt"] for i in filtered_data]
+            figure = px.line(x=date, y= temp, labels={"x": "Dates", "y": "Temperature (C)"})
+            sd.plotly_chart(figure)
+
+        if option == "Sky":
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {'Clear': 'images/clear.png', 'Clouds':'images/cloud.png',
+                      'Rain': 'images/rain.png', 'Snow':'images/snow.png'}
+            image_path = [images[condition] for condition in sky_condition]
+            sd.image(image_path, width=120)
+    except KeyError:
+        sd.write("Place does not exist.")
