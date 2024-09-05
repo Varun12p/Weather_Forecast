4886469f12e1f4c3b2e8c8797e30c9d7d123cf3e
Varun <panjwanivarun12@gmail.com>
2024-09-05 12:49:52 -0400

    filtered data from the json list .

--- backend.py
+++ backend.py
@@ -6,7 +6,14 @@ def get_data(place,days=None,option=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * days
+    filtered_data = filtered_data[:nr_values]
+    if option == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if option == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", days=3, option="Temperature"))
